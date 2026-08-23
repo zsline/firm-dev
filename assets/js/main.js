c2b3bc6d522880bdbc88423c0af1f9a49bb5316b
zsline <sergei.dereka@gmail.com>
2026-08-23 17:39:19 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+const heroSwiper = new Swiper('.hero__slider', {
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.hero__pagination',
+  },
+  navigation: {
+    nextEl: '.hero-button-next',
+    prevEl: '.hero-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+const slider = document.querySelector('.hero__slider');
+slider.addEventListener('mouseenter', () => {
+  heroSwiper.autoplay.stop();
+});
+slider.addEventListener('mouseleave', () => {
+  heroSwiper.autoplay.start();
+});
+
+
+// главная Hero форма
+const heroForm = document.querySelector('.hero__form');
+const heroFormButton = document.querySelector('.hero__form--label');
+
+if(heroForm || heroFormButton){
+  heroFormButton.addEventListener('click', () => {
+    heroForm.classList.toggle('is_open');
+  });
+}
